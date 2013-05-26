da21fa6e4aa1c8dd71409503e7e6c66f36fe8fd9
Olabiyi Oyewumi <usc0149@gmail.com>
2013-05-25 17:34:04 -0700

    instantiate prefab

--- instaniation.cs
+++ instaniation.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 using System.Collections;
   
 public class instantiation : MonoBehaviour {
+	
 	public GameObject earth;
 	
 void Start ()
 {
-Instantiate(earth,new Vector3(0,0,0), Quaternion.identity);
+Instantiate(Resources.Load("prefab/--"),new Vector3(0,0,0), Quaternion.identity);
 		
 }
 	void Update() 
 	{
-		Instantiate(earth, new Vector3(1,1,1), Quaternion.identity);
+		Instantiate(Resources.Load("prefab/--"),new Vector3(1,1,1), Quaternion.identity);
 		
     }
 }
