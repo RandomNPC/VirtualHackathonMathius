f7a7ead34759bbaee0fe2a1958d5ec382f6b844b
Paul Matias <pbmatias@ucdavis.edu>
2013-07-19 14:05:54 -0700

    Sound Manager module
    Added SoundManager.cs
    
    Notes:
    -Add sounds in Inspector (after adding script to a GameObject)
    -Two methods:
    setVolume( volume : float)
    playSound(sound_name : string, position : Vector3)
    -PLAY_ON_GAMEOBJECT (play sound on the GameObject that has this script
    attached.

---
+++ New Mathius Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+	
+	public AudioClip[] sounds;
+	private Dictionary<string,AudioClip> audioMapping;
+	private float audioVolume;
+	
+	public static Vector3 PLAY_ON_GAMEOBJECT;
+	
+	void Start(){
+		PLAY_ON_GAMEOBJECT = gameObject.transform.position;
+		audioVolume = 1.0f;
+		audioMapping = new Dictionary<string, AudioClip>();
+		audioMapping.Clear();
+		foreach(AudioClip ac in sounds){
+			audioMapping.Add(ac.name,ac);
+		}
+	}
+	
+	void Update(){
+		 PLAY_ON_GAMEOBJECT = gameObject.transform.position;
+	}
+	
+	public void setVolume(float volume){ audioVolume = volume;}
+	
+	public void playSound(string name, Vector3 position){
+		AudioClip playclip = null;
+		try{
+	 		playclip = audioMapping[name];
+		}
+		catch{
+			print ("AudioClip " + name + " does not exist");
+			return;
+		}
+		AudioSource.PlayClipAtPoint(playclip,position,audioVolume);
+	}
+}
